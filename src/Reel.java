61017696cd0e0e6ac0c527923b771c04bab53264
Goose-GitHub <Zackery.Kissinger@Outlook.Com>
2020-08-03 10:17:43 -0700

    Created a skeleton for the Reel class.

---
+++ src/Reel.java
@@ -0,0 +1,9 @@
+/* The Reel class contains the random number generator used to provide payout to the player.
+ * Reel also pulls from the Symbol class to push an icon to the player via the console window (front-end).
+ */
+
+public class Reel {
+    public static void main(String[] args) {
+        // Currently a skeleton! >:D
+    }
+}
--- src/TestClassGitHubZack.java
+++
@@ -1,6 +0,0 @@
-public class TestClassGitHubZack {
-    public static void main(String[] args) {
-        System.out.println("It's a trap!");
-        System.out.println("Also a trap!");
-    }
-}
